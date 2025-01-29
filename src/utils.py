cf5eb2241c12649913f1fea594a62836b0f82d24
Vinesh Patil <vineshpatil1054@gmail.com>
2025-01-30 01:59:58 +0530

    HyperParameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -22,21 +22,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]] #HT
 
-            # gs = GridSearchCV(model,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3) #gridsearchcv, i can also appy randomsearchcv
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
-            # model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            model.fit(X_train, y_train)  # Train model
+            # model.fit(X_train, y_train)  # Train model
+            #no need of above line after hyperparameter tuning - gridsearchcv
 
             y_train_pred = model.predict(X_train)
 
